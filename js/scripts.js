de2303d98d8173a89633f9ceeead75a39d95032b
Gary Bermudez <garybm.17@gmail.com>
2019-02-01 09:31:47 -0800

    add the form code in the HTML

--- js/scripts.js
+++ js/scripts.js
@@ -5,10 +5,26 @@
 //Create a prototype method for the cost of a pizza depending on the selections chosen. Use your own formula for this.
 //Keep it simple to start! You do not need to have a complex formula for determining cost (although you can if you like). The most important part of this code review is using and demonstrating your understanding of constructors and prototypes.//
 
-function Pizza(size, topping) {
+//bussines logic//
+function Pizza(size, toppings, prize) {
   this.size =  size;
-  this.topping = []; //This empty array will hold toppins//
+  this.toppings = []; //This empty array will hold toppins//
 }
 
-let order = new Pizza("medium", ["salami", "cheese"]);
-console.log.(order);
+Pizza.prototype.pizzaPrize = function() {
+  return this.prize;
+}
+
+// let order = new Pizza("medium", ["salami", "cheese"]);
+
+
+//UI logic//
+$(document).ready(function() {
+  $("form#new-pizza").submit(function(event) {
+    event.preventDefault();
+    var selectSize = $("input#size").val();
+    var toppings = $("input#toppings").val();
+
+    $("#newPizza").show();
+  })
+})
